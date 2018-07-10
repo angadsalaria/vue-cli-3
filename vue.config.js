b8d57e8cb934beadce518c380222c50916ed52b4
angadsalaria <>
2018-07-10 19:26:33 -0400

    move stuff to static (from public)

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
 var webpack = require('./vue.webpack.config.js')
 
 module.exports = {
-
   // Merged into the final Webpack config
-  configureWebpack: webpack.config
+  configureWebpack: webpack.config,
 
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = './static/index.html'
+        return args
+      })
+  },
+
+  pwa: {
+    themeColor: '#63AED8'
+  }
 }
