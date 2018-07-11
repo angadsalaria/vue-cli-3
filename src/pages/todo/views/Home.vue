446fa69f050d654d669862746d800b634f67c7be
angadsalaria <>
2018-07-10 23:20:22 -0400

    Add a new app (page) - Todo app

---
+++ src/pages/todo/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Todo msg="Welcome to Your Vue.js App!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Todo from '@/pages/todo/components/Todo.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Todo
+  }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -5,21 +5,6 @@ module.exports = {
   // Merged into the final Webpack config
   configureWebpack: webpack.config,
 
-  chainWebpack: config => {
-    config
-    .plugin('html-index')
-    .tap(args => {
-
-      console.log(args)
-
-      /*if(args.length>0){
-        args[0].template = './static/index.html'
-      }*/
-
-      return args
-    })
-  },
-
   pwa: {
     themeColor: '#63AED8'
   },
@@ -34,6 +19,11 @@ module.exports = {
       entry: 'src/pages/app/main.js',
       template: 'static/index.html',
       filename: 'index.html'
+    },
+    todo: {
+      entry: 'src/pages/todo/main.js',
+      template: 'static/index.html',
+      filename: 'todo.html'
     }
   }
 
